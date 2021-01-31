6fc04531e638d448bb800429c10dd0fc9fd24814
nikliza <75809540+nikliza@users.noreply.github.com>
2021-01-31 20:15:12 +0300

    Add files via upload

---
+++ week 1/ЧЕТНЫЕ ЧИСЛА/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a, b;
+    cin >> a >> b;
+    for (a ; a <= b ;++a )
+        if (a % 2 == 0) {
+            cout << a << ' ';
+            // x - чётное число
+        } else {
+            // x - нечётное число
+        }
+
+    return 0;
+}
+

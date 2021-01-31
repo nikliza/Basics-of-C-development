7bb5af1641b00642388b440556000f849080e639
nikliza <75809540+nikliza@users.noreply.github.com>
2021-01-31 20:52:05 +0300

    Add files via upload

---
+++ week 1/НАИБОЛЬШИЙ ОБЩИЙ ДЕЛИТЕЛЬ/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+
+    while (a > 0 && b > 0) {
+
+        if (a > b) {
+            a %= b;
+        } else {
+            b %= a;
+        }
+
+    }
+
+    cout << a + b;
+
+    return 0;
+}

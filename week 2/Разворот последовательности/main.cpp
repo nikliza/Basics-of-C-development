790805d7377650c289fc103d58107c39f36b17d5
nikliza <75809540+nikliza@users.noreply.github.com>
2021-02-08 00:02:42 +0300

    Add files via upload

---
+++ week 2/Разворот последовательности/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void Reverse(vector<int>& v){
+    int n = v.size();
+    for (int i = 0; i < n / 2; ++i){
+        int tmp = v[i];
+        v[i] = v[n - 1 - i];
+        v[n - 1 - i] = tmp;
+    }
+}
+
+int main() {
+
+    return 0;
+}

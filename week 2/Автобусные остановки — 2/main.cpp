87de63c38f5ca277961819d30eef051d5339be75
nikliza <75809540+nikliza@users.noreply.github.com>
2021-02-16 11:39:38 +0300

    Add files via upload

---
+++ week 2/Автобусные остановки — 2/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main(){
+    map<vector<string>,int> key_stop;
+    int q;
+    cin >> q;
+    int route_number = 1;
+    for (int i = 0; i<q; i++){
+        int number_teams;
+        cin >> number_teams;
+        vector<string> vec;
+        for (int j = 0; j < number_teams; j++){
+            string stop;
+            cin >> stop;
+            vec.push_back(stop);
+        }
+    if (key_stop.count(vec) == 1){
+        cout << "Already exists for "<< key_stop[vec]<< endl;
+        } else{
+        cout << "New bus " << route_number <<endl;
+        key_stop[vec] = route_number;
+        route_number++;
+        }
+    }
+
+
+    return 0;
+}
+
